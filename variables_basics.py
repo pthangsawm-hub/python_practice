b41e634141637bd3bf8070eaee6ea6c906021c08
P Thang Sawm <pthangsawm@gmail.com>
2026-06-26 16:14:38 +0700

    Add variables and data types practice (Week 1, Day 1)

---
+++ variables_basics.py
@@ -0,0 +1,16 @@
+# Practice: Variables & Data Types
+
+name = "Sawm Sawm"
+age = 25
+height = 5.9
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# Type conversion practice
+age_str = "25"
+age_num = int(age_str)
+print(age_num + 1)
